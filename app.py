2d5451466e25929486185aa7be3d66d8467738b7
Michał Szóstak <mszostakk01@gmail.com>
2020-04-04 22:38:58 +0200

    hotfix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, make_response, Response
 from flask_restful import Resource, Api
 import common.getAssingments as getAssingments
 import json
-import common.services.calendarAddon
+import common.services.calendarAddon as calendarAddon
 app = Flask(__name__)
 api = Api(app)
 
